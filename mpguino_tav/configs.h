FlashStopFall/MPGuino

commit 419973fb81e7e9d3ed486eb82919e8eb0f996e6e

numerous readability enhancements, re-worked decimal string converter, simplified debug monitor interpreter code

mpguino_tav/configs.h
@@ -40,11 +40,11 @@ const unsigned long myBaudRate1 = 19200ul;	// (Atmega2560 board)
 //
 #define trackIdleEOCdata true				// Ability to track engine idling and EOC modes
 #define useSpiffyTripLabels true			// Ability to use enhanced trip labels on main display screens
-//#define useSpiffyBigChars true
+#define useSpiffyBigChars true
 //#define useSoftwareClock true				// Shows 24 hour clock driven off of timer0, and provides a means to set it
-//#define useBigFE true						// Show big fuel economy displays
-//#define useBigDTE true						// Show big distance-to-empty displays
-//#define useBigTTE true						// Show big time-to-empty displays
+#define useBigFE true						// Show big fuel economy displays
+#define useBigDTE true						// Show big distance-to-empty displays
+#define useBigTTE true						// Show big time-to-empty displays
 //#define useBarFuelEconVsTime true			// Show Fuel Economy over Time bar graph
 //#define useBarFuelEconVsSpeed true			// Show Fuel Economy vs Speed, Fuel Used vs Speed bar graphs
 #define usePartialRefuel true				// Provide means to enter partial refuel amount into MPGuino
@@ -53,7 +53,7 @@ const unsigned long myBaudRate1 = 19200ul;	// (Atmega2560 board)
 #define useCPUreading true					// Show CPU loading and available RAM usage
 //#define useChryslerMAPCorrection true		// Ability to perform on-the-fly fuel injector data correction for late-model Chrysler vehicles
 //#define useChryslerBaroSensor true			// allows use of a separate MAP sensor wired to MPGuino to read barometric pressure, for even more accurate correction
-//#define useExpansionPins true				// Generate analog 0-5VDC output voltage on expansion pins to drive LEDs or feed signal to external gauges
+#define useOutputPins true					// Generate analog 0-5VDC output voltage on expansion pins to drive LEDs or feed signal to external gauges
 //#define blankScreenOnMessage true			// Completely blank display screen upon display of message
 //#define useImperialGallon true				// when selected, uses Imperial gallons instead of default US gallons
 //#define useCarVoltageOutput true			// Ability to display alternator voltage and optional secondary sensor (via meelis11)
@@ -96,12 +96,13 @@ const unsigned long myBaudRate1 = 19200ul;	// (Atmega2560 board)
 // program measurement and debugging tools
 //
 #define useDebugTerminal true					// (inw) terminal interface between PC and MPGuino
+//#define useDebugTerminalHelp true				// entering '?' in debug terminal displays brief help
 #define useDebugTerminalLabels true				// nice labels for various terminal interface output lists - requires useDebugTerminal
 #define useDebugCPUreading true					// Show enhanced CPU loading and available RAM usage - requires useDebugTerminal
 //#define useSWEET64trace true					// Ability to view real-time 64-bit calculations from SWEET64 kernel - requires useDebugTerminal
 //#define useTestButtonValues true				// Allows observation of button mapping
 #define useSimulatedFIandVSS true				// forces simulation of VSS and fuel injector events
-//#define useStatusLEDpins true					// allows usage of LEDs to show status of certain bits - currently used to indicate when MPGuino is awake
+#define useActivityLED true						// allows usage of LEDs to show status of certain bits - currently used to indicate when MPGuino is awake
 #define useDebugAnalog true						// forces ADC support to be compiled in, along with a dedicated analog screen
 //#define useObsoleteMult64 true					// use obsolete slower bit-based 64-bit multiplication
 //#define useSWEET64mult true						// shift mult64 from native C++ to SWEET64 bytecode (saves 36 bytes)
@@ -179,7 +180,7 @@ const unsigned long myBaudRate1 = 19200ul;	// (Atmega2560 board)
 #undef useAnalogMuxButtons
 #undef useParallax5PositionSwitch
 #undef useAdafruitRGBLCDshield
-#undef useStatusLEDpins
+#undef useActivityLED
 #endif // defined (useJellyBeanDriverBoard) || defined (useLegacyBoard)
 
 #if not(__AVR_ATmega32U4__) && not(__AVR_ATmega2560__) && not(__AVR_ATmega328P__)
@@ -401,7 +402,7 @@ const unsigned long myBaudRate1 = 19200ul;	// (Atmega2560 board)
 #define useTimer1 true
 #endif // useTimer1Interrupt
 
-#ifdef useExpansionPins
+#if defined(useOutputPins)
 #if defined(__AVR_ATmega32U4__)
 #define useTimer4 true
 #endif // defined(__AVR_ATmega32U4__)
@@ -412,7 +413,7 @@ const unsigned long myBaudRate1 = 19200ul;	// (Atmega2560 board)
 #define useTimer1 true
 #define useTimer2 true
 #endif // defined(__AVR_ATmega328P__)
-#endif // useExpansionPins
+#endif // defined(useOutputPins)
 
 #ifdef useDragRaceFunction
 #define useVehicleMass true
@@ -526,9 +527,9 @@ const unsigned long myBaudRate1 = 19200ul;	// (Atmega2560 board)
 #endif // useSerial0Port && __AVR_ATmega32U4__
 
 // Really, dcb? Really? You couldn't have used something else for LCD data bit 3?
-#if useLegacyLCD && __AVR_ATmega328P__ && useStatusLEDpins
-#error *** CANNOT use useLegacyLCD and useStatusLEDpins!!! ***
-#endif // useLegacyLCD && __AVR_ATmega328P__ && useStatusLEDpins
+#if useLegacyLCD && __AVR_ATmega328P__ && useActivityLED
+#error *** CANNOT use useLegacyLCD and useActivityLED!!! ***
+#endif // useLegacyLCD && __AVR_ATmega328P__ && useActivityLED
 
 #if useSoftwareClock && useDeepSleep
 #error *** CANNOT use both useSoftwareClock and useDeepSleep!!! ***

mpguino_tav/functions.h
@@ -614,20 +614,23 @@ static const uint8_t prgmAlternatorChannel[] PROGMEM = {
 static const uint8_t prgmRoundOffNumber[] PROGMEM = {
 	instrTestReg, 0x02,									// test register 2
 	instrBranchIfOverflow, 23,							// if register 2 has overflow value, exit
+	instrCmpIndex, 2,									// check if 3 or more right hand digits were specified
+	instrBranchIfGT, 17,								// if so, just exit
+	instrBranchIfE, 12,									// if 2 right hand digits were specified, round to nearest 100th
 	instrCmpIndex, 1,									// check if 0 or 1 right-hand digits were specified
-	instrBranchIfLT, 11,								// branch if 0 right-hand digits were specified
-	instrBranchIfE, 14,									// branch if 1 right-hand digit was specified
-	instrCmpIndex, 2,									// check if 2 right-hand digits were specified
-	instrBranchIfGT, 13,								// skip if 3 or more right-hand digits were specified
-	instrAddByteToX, 0x02, 5,							// round off to nearest 1/100th
-	instrSkip, 8,										// skip to exit
+	instrBranchIfE, 4,									// if 1 right hand digit was specified, round to nearest 10th
+	instrDiv2byConst, idxDecimalPoint,					// shift number right 3 digits to round to nearest whole digit
+	instrSkip, 6,										// skip to adjustment
 
-//roundint:
-	instrAddConstToX, 0x02, idxNumber500,				// round off to nearest whole integer
-	instrSkip, 3,										// skip to exit
+//to10ths:
+	instrDiv2byByte, 100,								// shift number right 2 digits
+	instrSkip, 2,										// skip to adjustment
 
-//round10th:
-	instrAddByteToX, 0x02, 50,							// round off to nearest 1/10th
+//to100ths:
+	instrDiv2byByte, 10,								// shift number right 1 digit
+
+//adjust:
+	instrAdjustQuotient,								// bump up quotient by adjustment term (0 if remainder/divisor < 0.5, 1 if remainder/divisor >= 0.5)
 
 //exit:
 	instrJump, tFormatToNumber							// go call prgmFormatToNumber to perform actual formatting
@@ -904,51 +907,6 @@ static const uint8_t prgmPressureChannel[] PROGMEM = {
 //cont1:
 	instrLdRegMainOffset, 0x02, mpMAPpressureIdx,		// load pre-formatted pressure element
 	instrDone											// exit to caller
-};
-
-static const uint8_t prgmCalculateMAPpressure[] PROGMEM = {
-	instrLdRegVoltage, 0x02, analogMAPchannelIdx,		// load analog channel ADC step value
-	instrSubMainFromX, 0x02, mpAnalogMAPfloorIdx,		// is reading below MAP sensor voltage floor?
-	instrBranchIfLT, 3,								// if not, continue
-	instrLdRegByte, 0x02, 0,							// zero out result in register 2
-
-//cont1:
-	instrMul2byMain, mpAnalogMAPnumerIdx,				// perform conversion to get pressure units per volts value
-	instrDiv2byMain, mpAnalogMAPdenomIdx,				// divide by pressure units per volts value
-	instrAddEEPROMtoX, 0x02, pMAPsensorOffsetIdx,		// add pressure offset value from EEPROM
-	instrStRegMain, 0x02, mpMAPpressureIdx,				// store resulting MAP sensor reading
-#ifdef useChryslerBaroSensor
-	instrDone											// exit to caller
-};
-
-static const uint8_t prgmCalculateBaroPressure[] PROGMEM = {
-	instrLdRegVoltage, 0x02, analogBaroChannelIdx,		// load analog channel ADC step value
-	instrSubMainFromX, 0x02, mpAnalogBaroFloorIdx,		// is reading below barometric sensor voltage floor?
-	instrBranchIfLT, 3,								// if not, continue
-	instrLdRegByte, 0x02, 0,							// zero out result in register 2
-
-//cont1:
-	instrMul2byMain, mpAnalogBaroNumerIdx,				// convert to obtain pressure units per volts value
-	instrDiv2byMain, mpAnalogBaroDenomIdx,				// divide by pressure units per volts value
-	instrAddEEPROMtoX, 0x02, pBaroSensorOffsetIdx,		// add pressure offset value from EEPROM
-	instrStRegMain, 0x02, mpBaroPressureIdx,			// store resulting barometric sensor reading
-#endif // useChryslerBaroSensor
-	instrLdRegMain, 0x02, mpFuelPressureIdx,			// get fuel system differential pressure
-	instrAddMainToX, 0x02, mpBaroPressureIdx,			// add to reference barometric pressure to get fuel system absolute pressure
-	instrSubMainFromX, 0x02, mpMAPpressureIdx,			// subtract MAP to get differential pressure across the fuel injector
-	instrStRegMain, 0x02, mpInjPressureIdx,				// store differential pressure across the fuel injector
-	instrMul2byConst, idxCorrectionFactor,				// set up for iSqrt
-	instrDiv2byMain, mpFuelPressureIdx,					// divide by the fuel system differential pressure
-	instrTestReg, 0x02,									// test whether overflow occurred
-	instrBranchIfOverflow, 6,							// if overflow occurred, go handle it
-	instrIsqrt, 0x02,									// perform square root on result
-	instrStRegVolatile, 0x02, vInjectorCorrectionIdx,	// save square root of presssure differential ratio as fuel injector correction factor
-	instrDone,											// return to caller
-
-//cont3:
-	instrLdRegConst, 0x02, idxCorrectionFactor,
-	instrStRegVolatile, 0x02, vInjectorCorrectionIdx,	// save initial injector correction index for pressure differential calculation
-	instrDone											// return to caller
 };
 
 #endif // useChryslerMAPCorrection

mpguino_tav/trip_measurement.h
@@ -1,8 +1,3 @@
-#ifdef useWindowTripFilter
-void windowTripFilterReset(void);
-#endif // useWindowTripFilter
-static uint8_t translateTripIndex(const uint8_t tripTranslateList[], uint8_t tripListPos);
-
 namespace tripVar /* trip data collection basic function prototype */
 {
 
@@ -21,6 +16,9 @@ namespace tripVar /* trip data collection basic function prototype */
 namespace tripSupport /* Trip save/restore/reset support section prototype */
 {
 
+	static void init(void);
+	static void idleProcess(void);
+	static uint8_t translateTripIndex(const uint8_t tripTranslateList[], uint8_t tripListPos);
 #ifdef useSavedTrips
 	static uint8_t displayHandler(uint8_t cmd, uint8_t cursorPos, uint8_t cursorChanged);
 	static void goSaveCurrent(void);
@@ -34,6 +32,9 @@ namespace tripSupport /* Trip save/restore/reset support section prototype */
 	static void doResetTrip(uint8_t tripSlot);
 	static void resetCurrent(void);
 	static void resetTank(void);
+#ifdef useWindowTripFilter
+	static void resetWindowFilter(void);
+#endif // useWindowTripFilter
 
 };
 
@@ -44,6 +45,10 @@ namespace pressureCorrect /* Chrysler returnless fuel pressure correction displa
 	static uint8_t displayHandler(uint8_t cmd, uint8_t cursorPos, uint8_t cursorChanged);
 
 }
+
+const char pressureCorrectScreenFuncNames[] PROGMEM = {
+	"Pressures\r"
+};
 
 #endif // useChryslerMAPCorrection
 const uint8_t rvVSSpulseIdx = 0; 			// distance pulse count
@@ -377,9 +382,3 @@ const char tripSaveFuncNames[] PROGMEM = {
 };
 
 #endif // useSavedTrips
-#ifdef useChryslerMAPCorrection
-const char pressureCorrectScreenFuncNames[] PROGMEM = {
-	"Pressures\r"
-};
-
-#endif // useChryslerMAPCorrection

commit e3a1791cd7b0d2964691584089c690610b5fd341

Fixed useChryslerMapCorrection and a #include <avr/eeprom.h> typo.
ChryslerMapCorrection was broken when refactoring code in commit 419973f (Jun-6-2023). Code that used to be in functions.h got moved to trip_measurement.ino, and code that used to be in trip_measurement.ino got moved to trip_measurement.h. Moving them back allows useChryslerMapCorrection to compile.

mpguino_tav/configs.h
@@ -15,7 +15,7 @@
 //   - useJSONoutput outputs selected trip functions as a JSON array of JSON objects
 //   - useDebugTerminal provides for a PC-MPGuino debugging environment to test various basic MPGuino functions
 //
-//#define useDataLoggingOutput true			// output 5 basic trip functions to a data logger or SD card, once every refresh period (0.5 second)
+#define useDataLoggingOutput true			// output 5 basic trip functions to a data logger or SD card, once every refresh period (0.5 second)
 //#define useJSONoutput true					// skybolt added to enable and call JSON out routine
 //#define useDebugTerminal true				// debugging terminal interface between PC and MPGuino
 //#define useBluetooth true					// bluetooth interface with Android phone
@@ -24,7 +24,7 @@
 //   - if useDataLoggingOutput is not selected, the below output port options will be ignored
 //   - if useDataLoggingOutput is selected, choose only one of the below output port options, or an error will result
 //
-//#define useLoggingSerialPort0 true			// select logging output on serial port channel 0 (most Arduino boards, excluding TinkerKit! LCD module)
+#define useLoggingSerialPort0 true			// select logging output on serial port channel 0 (most Arduino boards, excluding TinkerKit! LCD module)
 //#define useLoggingSerialPort1 true			// select logging output on serial port channel 1 (ATmega2560 board, Atmega32U4 board excluding TinkerKit! LCD module)
 //#define useLoggingSerialPort2 true			// select logging output on serial port channel 2 (ATmega2560 board)
 //#define useLoggingSerialPort3 true			// select logging output on serial port channel 3 (ATmega2560 board)
@@ -74,7 +74,7 @@
 //
 // if either useTinkerkitLCDmodule or useMPGuinoColourTouch is used, the below options will be ignored
 //
-//#define useLegacyLCD true					// select Legacy 16x2 4-bit LCD
+#define useLegacyLCD true					// select Legacy 16x2 4-bit LCD
 //#define useDFR0009LCD true						// (inw) select DFRobot DFR0009 LCD Keypad Shield
 //#define useAdafruitRGBLCDshield true		// select Adafruit RGB 16x2 4-bit LCD module over TWI
 //#define useParallaxSerialLCDmodule true		// select Parallax 16x2 Serial LCD module
@@ -103,7 +103,7 @@
 // note: if useAdafruitRGBLCDshield is selected, useLegacyButtons will be ignored
 //       if useMPGuinoColourTouch is selected, all hardware button choices will be ignored
 //
-//#define useLegacyButtons true
+#define useLegacyButtons true
 //#define useAnalogMuxButtons true
 //#define useParallax5PositionSwitch true
 
@@ -112,21 +112,21 @@
 #define trackIdleEOCdata true				// Ability to track engine idling and EOC modes
 #define useSavedTrips true					// Ability to save current or tank trips to EEPROM
 #define usePartialRefuel true				// Provide means to enter partial refuel amount into MPGuino
-//#define useFuelCost true					// Show fuel cost
-//#define useDragRaceFunction true			// Performs "drag race" 0-60 MPH, 1/4 mile time, estimated horsepower functionality
-//#define useBigFE true						// Show big fuel economy displays
-//#define useBigDTE true						// Show big distance-to-empty displays
-//#define useBigTTE true						// Show big time-to-empty displays
-//#define useBarFuelEconVsTime true			// Show Fuel Economy over Time bar graph
-//#define useBarFuelEconVsSpeed true			// Show Fuel Economy vs Speed, Fuel Used vs Speed bar graphs
-//#define useStatusMeter true					// displays a graphical meter for use with MPG display
-//#define useSpiffyTripLabels true			// Ability to use enhanced trip labels on main display screens
+#define useFuelCost true					// Show fuel cost
+#define useDragRaceFunction true			// Performs "drag race" 0-60 MPH, 1/4 mile time, estimated horsepower functionality
+#define useBigFE true						// Show big fuel economy displays
+#define useBigDTE true						// Show big distance-to-empty displays
+#define useBigTTE true						// Show big time-to-empty displays
+#define useBarFuelEconVsTime true			// Show Fuel Economy over Time bar graph
+#define useBarFuelEconVsSpeed true			// Show Fuel Economy vs Speed, Fuel Used vs Speed bar graphs
+#define useStatusMeter true					// displays a graphical meter for use with MPG display
+#define useSpiffyTripLabels true			// Ability to use enhanced trip labels on main display screens
 #define useSpiffyBigChars true				// Provides better number font with use with big number displays above
-//#define useScreenEditor true				// Ability to change any of (9 or 12, depending on configuration) existing trip data screens, with 4 configurable figures on each screen
-//#define useSoftwareClock true				// Shows 24 hour clock driven off of timer0, and provides a means to set it
+#define useScreenEditor true				// Ability to change any of (9 or 12, depending on configuration) existing trip data screens, with 4 configurable figures on each screen
+#define useSoftwareClock true				// Shows 24 hour clock driven off of timer0, and provides a means to set it
 //#define useCPUreading true					// Show CPU loading and available RAM usage
-//#define useChryslerMAPCorrection true		// Ability to perform on-the-fly fuel injector data correction for late-model Chrysler vehicles
-//#define useChryslerBaroSensor true			// allows use of a separate MAP sensor wired to MPGuino to read barometric pressure, for even more accurate correction
+#define useChryslerMAPCorrection true		// Ability to perform on-the-fly fuel injector data correction for late-model Chrysler vehicles
+#define useChryslerBaroSensor true			// allows use of a separate MAP sensor wired to MPGuino to read barometric pressure, for even more accurate correction
 //#define useOutputPins true					// Generate analog 0-5VDC output voltage on expansion pins to drive LEDs or feed signal to external gauges
 //#define blankScreenOnMessage true			// Completely blank display screen upon display of status message
 //#define useImperialGallon true				// when selected, uses Imperial gallons instead of default US gallons

mpguino_tav/functions.h
@@ -942,6 +942,51 @@ static const uint8_t prgmPressureChannel[] PROGMEM = {
 	instrDone											// exit to caller
 };
 
+static const uint8_t prgmCalculateMAPpressure[] PROGMEM = {
+	instrLdRegVoltage, 0x02, analogMAPchannelIdx,		// load analog channel ADC step value
+	instrSubMainFromX, 0x02, mpAnalogMAPfloorIdx,		// is reading below MAP sensor voltage floor?
+	instrBranchIfLT, 3,									// if not, continue
+	instrLdRegByte, 0x02, 0,							// zero out result in register 2
+
+//cont1:
+	instrMul2byMain, mpAnalogMAPnumerIdx,				// perform conversion to get pressure units per volts value
+	instrDiv2byMain, mpAnalogMAPdenomIdx,				// divide by pressure units per volts value
+	instrAddEEPROMtoX, 0x02, pMAPsensorOffsetIdx,		// add pressure offset value from EEPROM
+	instrStRegMain, 0x02, mpMAPpressureIdx,				// store resulting MAP sensor reading
+#if defined(useChryslerBaroSensor)
+	instrDone											// exit to caller
+};
+
+static const uint8_t prgmCalculateBaroPressure[] PROGMEM = {
+	instrLdRegVoltage, 0x02, analogBaroChannelIdx,		// load analog channel ADC step value
+	instrSubMainFromX, 0x02, mpAnalogBaroFloorIdx,		// is reading below barometric sensor voltage floor?
+	instrBranchIfLT, 3,									// if not, continue
+	instrLdRegByte, 0x02, 0,							// zero out result in register 2
+
+//cont1:
+	instrMul2byMain, mpAnalogBaroNumerIdx,				// convert to obtain pressure units per volts value
+	instrDiv2byMain, mpAnalogBaroDenomIdx,				// divide by pressure units per volts value
+	instrAddEEPROMtoX, 0x02, pBaroSensorOffsetIdx,		// add pressure offset value from EEPROM
+	instrStRegMain, 0x02, mpBaroPressureIdx,			// store resulting barometric sensor reading
+#endif // defined(useChryslerBaroSensor)
+	instrLdRegMain, 0x02, mpFuelPressureIdx,			// get fuel system differential pressure
+	instrAddMainToX, 0x02, mpBaroPressureIdx,			// add to reference barometric pressure to get fuel system absolute pressure
+	instrSubMainFromX, 0x02, mpMAPpressureIdx,			// subtract MAP to get differential pressure across the fuel injector
+	instrStRegMain, 0x02, mpInjPressureIdx,				// store differential pressure across the fuel injector
+	instrMul2byConst, idxCorrectionFactor2,				// set up for iSqrt
+	instrDiv2byMain, mpFuelPressureIdx,					// divide by the fuel system differential pressure
+	instrTestReg, 0x02,									// test whether overflow occurred
+	instrBranchIfOverflow, 6,							// if overflow occurred, go handle it
+	instrIsqrt, 0x02,									// perform square root on result
+	instrStRegVolatile, 0x02, vInjectorCorrectionIdx,	// save square root of presssure differential ratio as fuel injector correction factor
+	instrDone,											// return to caller
+
+//cont3:
+	instrLdRegConst, 0x02, idxCorrectionFactor,
+	instrStRegVolatile, 0x02, vInjectorCorrectionIdx,	// save initial injector correction index for pressure differential calculation
+	instrDone											// return to caller
+};
+
 #endif // defined(useChryslerMAPCorrection)
 #if defined(useDragRaceFunction)
 static const uint8_t prgmDragSpeed[] PROGMEM = {		// tDragSpeed - acceleration test maximum vehicle speed (SI/SAE)

mpguino_tav/mpguino_tav.ino
@@ -418,7 +418,7 @@ Logging Output / Debug Monitor I/O
 
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
-#include <avr/EEPROM.h>
+#include <avr/eeprom.h>
 #include <avr/sleep.h>
 
 static const char titleMPGuino[] PROGMEM = {

mpguino_tav/trip_measurement.h
@@ -434,13 +434,6 @@ static const char pressureCorrectDisplayTitles[] PROGMEM = {
 	"Pressures" tcEOS
 };
 
-static const uint16_t pressureCorrectPageFormats[4] PROGMEM = {
-	 ((mpMAPpressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)		// Pressures
-	,((mpBaroPressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)
-	,((mpFuelPressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)
-	,((mpInjPressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)
-};
-
 #endif // defined(useChryslerMAPCorrection)
 #if defined(useEnhancedTripReset)
 namespace tripSave /* Trip save/restore/reset display support section prototype */

mpguino_tav/trip_measurement.ino
@@ -666,49 +666,11 @@ static void tripSupport::resetWindowFilter(void)
 #if defined(useChryslerMAPCorrection)
 /* Chrysler returnless fuel pressure correction display section */
 
-static const uint8_t prgmCalculateMAPpressure[] PROGMEM = {
-	instrLdRegVoltage, 0x02, analogMAPchannelIdx,		// load analog channel ADC step value
-	instrSubMainFromX, 0x02, mpAnalogMAPfloorIdx,		// is reading below MAP sensor voltage floor?
-	instrBranchIfLT, 3,									// if not, continue
-	instrLdRegByte, 0x02, 0,							// zero out result in register 2
-
-//cont1:
-	instrMul2byMain, mpAnalogMAPnumerIdx,				// perform conversion to get pressure units per volts value
-	instrDiv2byMain, mpAnalogMAPdenomIdx,				// divide by pressure units per volts value
-	instrAddEEPROMtoX, 0x02, pMAPsensorOffsetIdx,		// add pressure offset value from EEPROM
-	instrStRegMain, 0x02, mpMAPpressureIdx,				// store resulting MAP sensor reading
-#if defined(useChryslerBaroSensor)
-	instrDone											// exit to caller
-};
-
-static const uint8_t prgmCalculateBaroPressure[] PROGMEM = {
-	instrLdRegVoltage, 0x02, analogBaroChannelIdx,		// load analog channel ADC step value
-	instrSubMainFromX, 0x02, mpAnalogBaroFloorIdx,		// is reading below barometric sensor voltage floor?
-	instrBranchIfLT, 3,									// if not, continue
-	instrLdRegByte, 0x02, 0,							// zero out result in register 2
-
-//cont1:
-	instrMul2byMain, mpAnalogBaroNumerIdx,				// convert to obtain pressure units per volts value
-	instrDiv2byMain, mpAnalogBaroDenomIdx,				// divide by pressure units per volts value
-	instrAddEEPROMtoX, 0x02, pBaroSensorOffsetIdx,		// add pressure offset value from EEPROM
-	instrStRegMain, 0x02, mpBaroPressureIdx,			// store resulting barometric sensor reading
-#endif // defined(useChryslerBaroSensor)
-	instrLdRegMain, 0x02, mpFuelPressureIdx,			// get fuel system differential pressure
-	instrAddMainToX, 0x02, mpBaroPressureIdx,			// add to reference barometric pressure to get fuel system absolute pressure
-	instrSubMainFromX, 0x02, mpMAPpressureIdx,			// subtract MAP to get differential pressure across the fuel injector
-	instrStRegMain, 0x02, mpInjPressureIdx,				// store differential pressure across the fuel injector
-	instrMul2byConst, idxCorrectionFactor2,				// set up for iSqrt
-	instrDiv2byMain, mpFuelPressureIdx,					// divide by the fuel system differential pressure
-	instrTestReg, 0x02,									// test whether overflow occurred
-	instrBranchIfOverflow, 6,							// if overflow occurred, go handle it
-	instrIsqrt, 0x02,									// perform square root on result
-	instrStRegVolatile, 0x02, vInjectorCorrectionIdx,	// save square root of presssure differential ratio as fuel injector correction factor
-	instrDone,											// return to caller
-
-//cont3:
-	instrLdRegConst, 0x02, idxCorrectionFactor,
-	instrStRegVolatile, 0x02, vInjectorCorrectionIdx,	// save initial injector correction index for pressure differential calculation
-	instrDone											// return to caller
+static const uint16_t pressureCorrectPageFormats[4] PROGMEM = {
+	 ((mpMAPpressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)		// Pressures
+	,((mpBaroPressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)
+	,((mpFuelPressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)
+	,((mpInjPressureIdx - mpMAPpressureIdx) << 8 ) |	(tPressureChannel)
 };
 
 static uint8_t pressureCorrect::displayHandler(uint8_t cmd, uint8_t cursorPos)